da1820cebddb482a519b9e454fc2f28600302463
shalevzuriel <shalevz@odem.pro>
2023-11-25 11:26:38 +0200

    change the BotHardware class to singleton in order to work with the same instance from all classes.

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveTrain.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveTrain.java
@@ -4,5 +4,4 @@ public class DriveTrain {
 
     //TODO: method to move the robot by mecanum wheels robot-centeric. meaning, a method which it's parameters are turn, forward and side. by those parameters the robot will move.
 
-
 }
