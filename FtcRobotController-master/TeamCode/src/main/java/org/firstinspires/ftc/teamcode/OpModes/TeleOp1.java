c67154b9fcbb56e39fed1ac3eeb916d41f15c2f9
Nisgush <noamn@odem.pro>
2023-11-26 21:52:49 +0200

    Added Vector class Added inputs from controller on TeleOp1 Worked on Space-centeric driving

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp1.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp1.java
@@ -20,9 +20,13 @@ public class TeleOp1 extends LinearOpMode {
         while (opModeIsActive())
         {
             //TODO Get values from controller that are necessary either for driving or moving the arm
+            float leftStickX = gamepad1.left_stick_x;
+            float leftStickY = gamepad1.left_stick_y;
+            float rightStickX = gamepad1.right_stick_x;
+            float rightStickY = gamepad1.right_stick_y;
 
             //TODO Put the values from the controller into the relevant methods
-
+            
             //TODO update telemetrygit 
         }
     }
--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveTrain.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveTrain.java
@@ -1,8 +1,68 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class DriveTrain {
 
     //TODO: method to move the robot by mecanum wheels robot-centeric. meaning, a method which it's parameters are turn, forward and side. by those parameters the robot will move.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    DcMotor frontRight;
+    DcMotor frontLeft;
+    DcMotor rearRight;
+    DcMotor rearLeft;
+    public void power(Vector vector, float IMUAngle){
+        float radius = vector.getRadius();
+        float angle = vector.getAngle();
+        float movingAngle = angle - IMUAngle + 45;
+
+        frontRight.setPower(radius * Math.cos(movingAngle));
+        frontLeft.setPower(radius * Math.sin(movingAngle));
+        rearRight.setPower(-(radius * Math.cos(movingAngle)));
+        rearLeft.setPower(-(radius * Math.sin(movingAngle)));
+    }
 
 }
