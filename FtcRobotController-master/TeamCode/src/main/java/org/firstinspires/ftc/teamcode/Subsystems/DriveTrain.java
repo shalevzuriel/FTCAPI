77ab2fe7e70ae521533182b0a2a4aba1a62dffed
Arik Shapira <aries@odem.pro>
2023-11-27 16:45:49 +0200

    this is a drive train. yay! we have a drive train now. shalev approved.
    The code recieves 3 doubles, each between -1 and 1, representing different components of the robots movement vector.
    the method will then create a movement vector(how exciting).

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveTrain.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveTrain.java
@@ -9,13 +9,20 @@ public class DriveTrain {
      * based on the parameters you give it
      * @param y represents the y component of the movement vector, demands double between 1 and -1.
      * @param x represents the x component of the movement vector, demands double between 1 and -1,
-     * @param t represnts how mush of the movement vector will be allocated to torque, demands double between 1 and -1
+     * @param t represents how mush of the movement vector will be allocated to torque, demands double between 1 and -1
      */
-        public void robotCentricDrive(double y, double x, double t){
-
-            double denominator = Math.max(y + x + t, 1);
-
-        BH.setDrivePower((y - x + t) / denominator, (y + x - t) / denominator, (y - x - t) / denominator, (y + x + t) / denominator);
-        //further testing/fine tuning  is required to see whether this method works on our robot.
+    public void robotCentricDrive(double y, double x, double t){
+        //the denominator is put in place to scale down all the motors so the power value of the motors is allways between -1 and 1.
+        double denominator = Math.max(y + x + t, 1);
+        /* now the reasons for the following 4 lines of code are only vector based,
+         * so i've decided i just wont explain.
+         * here's a link to a thing:
+         * https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
+         */
+        double leftFrontWheel = (y + x + t) / denominator;
+        double leftRearWheel =  (y - x + t) / denominator;
+        double rightFrontWheel = (y - x - t) / denominator;
+        double rightRearWheel = (y + x - t) / denominator;
+        BH.setDrivePower(leftRearWheel, rightRearWheel, rightFrontWheel, leftFrontWheel);
     }
 }
