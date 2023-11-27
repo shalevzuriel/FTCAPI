5e318daa9e99509c4b2ecdcb90b29977c5febe12
Arik Shapira <aries@odem.pro>
2023-11-27 16:13:19 +0200

    מוסיף את הקוד של הגלגלים

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveTrain.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveTrain.java
@@ -3,16 +3,19 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 import org.firstinspires.ftc.teamcode.BotHardware;
 
 public class DriveTrain {
-    BotHardware BH = new BotHardware();
-    //TODO: method to move the robot by mecanum wheels robot-centeric. meaning, a method which it's
-    // parameters are turn, forward and side. by those parameters the robot will move.
-    public void robotCentricDrive(double y, double x, double t){
-        /**
-         * y represents the desired magnitude for the y component of the movement vector.
-         * x represents desired x component of the movement vector.
-         * t represents the desired torque.
-         */
-        BH.setDrivePower(y - x + t, y + x - t, y - x - t, y + x + t);
+    BotHardware BH = BotHardware.getInstance();
+    /**
+     * robotCentricDrive will set the robots motors to the right power to create a movement vector
+     * based on the parameters you give it
+     * @param y represents the y component of the movement vector, demands double between 1 and -1.
+     * @param x represents the x component of the movement vector, demands double between 1 and -1,
+     * @param t represnts how mush of the movement vector will be allocated to torque, demands double between 1 and -1
+     */
+        public void robotCentricDrive(double y, double x, double t){
+
+            double denominator = Math.max(y + x + t, 1);
+
+        BH.setDrivePower((y - x + t) / denominator, (y + x - t) / denominator, (y - x - t) / denominator, (y + x + t) / denominator);
         //further testing/fine tuning  is required to see whether this method works on our robot.
     }
 }
