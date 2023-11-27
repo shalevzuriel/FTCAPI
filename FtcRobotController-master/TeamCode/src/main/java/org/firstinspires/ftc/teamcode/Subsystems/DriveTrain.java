af98d7a44ed252f20c58889083a153fc0e57d0fa
Arik Shapira <aries@odem.pro>
2023-11-27 08:21:40 +0200

    מוסיף את הקוד של הגלגלים

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveTrain.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveTrain.java
@@ -1,8 +1,18 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import org.firstinspires.ftc.teamcode.BotHardware;
+
 public class DriveTrain {
-
-    //TODO: method to move the robot by mecanum wheels robot-centeric. meaning, a method which it's parameters are turn, forward and side. by those parameters the robot will move.
-
-
+    BotHardware BH = new BotHardware();
+    //TODO: method to move the robot by mecanum wheels robot-centeric. meaning, a method which it's
+    // parameters are turn, forward and side. by those parameters the robot will move.
+    public void robotCentricDrive(double y, double x, double t){
+        /**
+         * y represents the desired magnitude for the y component of the movement vector.
+         * x represents desired x component of the movement vector.
+         * t represents the desired torque.
+         */
+        BH.setDrivePower(y - x + t, y + x - t, y - x - t, y + x + t);
+        //further testing/fine tuning  is required to see whether this method works on our robot.
+    }
 }
