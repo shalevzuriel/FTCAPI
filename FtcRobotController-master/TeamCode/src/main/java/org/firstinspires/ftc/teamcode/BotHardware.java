dc959b6704b22852b730afb86747edbadac7bf0d
ronTheGoodGuy <rona@odem.pro>
2023-11-29 12:24:00 +0200

    Merge pull request #5 from shalevzuriel/users/shalev/armMotor
    adding Bot hardware of the arm motor

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotHardware.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotHardware.java
@@ -1,18 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.BHI260IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.hardware.rev.RevTouchSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.hardware.IMU;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
 public class BotHardware {
 
@@ -31,7 +27,7 @@ public class BotHardware {
     private Rev2mDistanceSensor mDistanceSensor = null;
     private RevColorSensorV3 colorSensor = null;
     private RevTouchSensor touchSensor =null;
-    private BNO055IMU imu = null;
+    private IMU imu;
 
     private boolean isInitCalled = false;
 
@@ -56,45 +52,44 @@ public class BotHardware {
      * All of the hardware devices are accessed via the hardware map, and initialized.
      */
     public void init(LinearOpMode myOpMode)    {
-        if (!isInitCalled) {
-            // Define and Initialize Motors (note: need to use reference to actual OpMode).
-            leftRearDrive = myOpMode.hardwareMap.get(DcMotor.class, "left_drive_rear");
-            rightRearDrive = myOpMode.hardwareMap.get(DcMotor.class, "right_drive_rear");
-            leftFrontDrive = myOpMode.hardwareMap.get(DcMotor.class, "left_drive_front");
-            rightFrontDrive = myOpMode.hardwareMap.get(DcMotor.class, "right_drive_front");
-            //define and Initialize Sensors
-            mDistanceSensor = myOpMode.hardwareMap.get(Rev2mDistanceSensor.class, "distance_sensor");
-            colorSensor = myOpMode.hardwareMap.get(RevColorSensorV3.class, "color_sensor");
-            touchSensor = myOpMode.hardwareMap.get(RevTouchSensor.class, "touch_sensor");
-            imu = myOpMode.hardwareMap.get(BNO055IMU.class, "imu");
+        // Define and Initialize Motors (note: need to use reference to actual OpMode).
+        leftRearDrive = myOpMode.hardwareMap.get(DcMotor.class, "left_drive_rear"); //port 1
+        rightRearDrive = myOpMode.hardwareMap.get(DcMotor.class, "right_drive_rear"); //port 3
+        leftFrontDrive = myOpMode.hardwareMap.get(DcMotor.class, "left_drive_front"); //port 0
+        rightFrontDrive = myOpMode.hardwareMap.get(DcMotor.class, "right_drive_front"); //port 2
+        //define and Initialize Sensors
+        /*
+        mDistanceSensor = myOpMode.hardwareMap.get(Rev2mDistanceSensor.class, "distance_sensor");
+        colorSensor = myOpMode.hardwareMap.get(RevColorSensorV3.class, "color_sensor");
+        touchSensor = myOpMode.hardwareMap.get(RevTouchSensor.class, "touch_sensor");
+         */
 
-            // setting of the units of measurements for angles
-            BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-            parameters.angleUnit =  BNO055IMU.AngleUnit.DEGREES;
-            parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-            parameters.calibrationDataFile = "BNO055IMUCalibration.json";
-            parameters.loggingEnabled = true;
-            parameters.loggingTag = "IMU";
-            parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
-            //initializing the imu with parameters
-            imu.initialize(parameters);
-            // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-            // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-            // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-            leftRearDrive.setDirection(DcMotor.Direction.REVERSE);
-            leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        //setting the logo facing directions as back and the usb facing directions as left (just as it is connected to the robot).
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD;
+        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.LEFT;
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
 
-            // If there are encoders connected, switch to RUN_USING_ENCODER mode for greater accuracy
-            // leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            // rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // Now initialize the IMU with this mounting orientation
+        // This sample expects the IMU to be in a REV Hub and named "imu".
+        imu = myOpMode.hardwareMap.get(IMU.class, "imu");
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+        imu.resetYaw();
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        leftRearDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
 
-            // Define and initialize ALL installed servos.
+        // If there are encoders connected, switch to RUN_USING_ENCODER mode for greater accuracy
+        // leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-            myOpMode.telemetry.addData(">", "Hardware Initialized");
-            myOpMode.telemetry.update();
-            isInitCalled = true;
-        }
+        // Define and initialize ALL installed servos.
+
+        myOpMode.telemetry.addData(">", "Hardware Initialized");
+        myOpMode.telemetry.update();
+        isInitCalled = true;
     }
 
     /**
@@ -120,5 +115,5 @@ public class BotHardware {
     public Rev2mDistanceSensor get2mDistanceSensor(){return mDistanceSensor;}
     public RevColorSensorV3 getColorSensor(){return colorSensor;}
     public RevTouchSensor getTouchSensor() {return touchSensor;}
-    public BNO055IMU getIMU(){return imu;}
+    public IMU getIMU(){return imu;}
 }
