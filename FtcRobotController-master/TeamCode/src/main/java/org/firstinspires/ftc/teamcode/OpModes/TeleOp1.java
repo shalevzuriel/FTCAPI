e6f2ccccfa384d8476b3dc496bb12c8a3a3f7146
Nisgush <noamn@odem.pro>
2023-11-29 17:19:30 +0200

    Integrated IMU into space-centric driving and finished coding it. Now drive in space-centric mode. Added the option to reset YAW(rotation) using "share" button. Giving the possibility for the driver to move around the room.

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp1.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp1.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.BotHardware;
 import org.firstinspires.ftc.teamcode.Subsystems.DriveTrain;
 
@@ -23,13 +24,23 @@ public class TeleOp1 extends LinearOpMode {
         while (opModeIsActive())
         {
             //TODO Get values from controller that are necessary either for driving or moving the arm
-            double leftStickX = -gamepad1.left_stick_x;
+            double leftStickX = gamepad1.left_stick_x;
             double leftStickY = -gamepad1.left_stick_y;
             double rightStickX = gamepad1.right_stick_x;
             double rightStickY = gamepad1.right_stick_y;
 
-            DriveTrain.spaceCentricMoving(leftStickX,leftStickY, rightStickX, 0);
-            botHardware.setDrivePower(0,0,0,0);
+            //Resetting the YAW(rotation)
+            if (gamepad1.share)
+            {
+                botHardware.getIMU().resetYaw();
+            }
+
+            //Getting IMU rotation
+            double IMUAngleRads = botHardware.getIMU().getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            DriveTrain.spaceCentricMoving(leftStickX,leftStickY, rightStickX, IMUAngleRads); //Print
+            telemetry.addData("Angle", Math.toDegrees(IMUAngleRads)); //Prints out the angle.
+            telemetry.update();
             //TODO Put the values from the controller into the relevant methods
 
             //TODO update telemetrygit 
